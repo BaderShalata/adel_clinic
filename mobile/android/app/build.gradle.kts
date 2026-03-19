5cfb5c1eb8f81af8c24c17df0e1d236a05e44395
BaderShalata <shalatab4@gmail.com>
2026-03-19 14:24:12 +0200

    Android Biometrics Login

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -61,4 +62,5 @@ flutter {
 }
 dependencies {
     implementation("androidx.appcompat:appcompat:1.4.0")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
